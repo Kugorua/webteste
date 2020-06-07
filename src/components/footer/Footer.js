6357d8bb1ab9c22a53e6048875e9e913d342521e
NQphuoc <nguyenqp97@gmail.com>
2020-06-07 11:11:11 +0700

    designs header footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './footer.scss';
+import imgG893 from '../../images/Group 893.png';
+import imgG979 from '../../images/mobievo.png';
+import imgNPlogo from '../../images/np_logo.png';
+import imgIconFb from '../../images/Component 1.png';
+import imgIconTw from '../../images/Component.png';
 export default function Footer() {
   return (
     <div className="footer grid-full">
       <div className="footer__wrap grid">
-        <img src={''} />
-        <a href={'#'}>https://corp.kinto-jp.com/</a>
-        <img src={''} />
-        <a href={'#'}>https://corp.kinto-jp.com/</a>
+        <div className="footer-top">
+          <img src={imgG893} />
+          <a href={'#'}>https://corp.kinto-jp.com/</a>
+          <img src={imgG979} />
+          <a href={'#'}>https://corp.kinto-jp.com/</a>
+        </div>
+        <div className="footer-share">
+          <p>SHARE THIS PAGE</p>
+          <a href={'#'}>
+            <img src={imgIconTw} />
+          </a>
+          <a href={'#'}>
+            <img src={imgIconFb} />
+          </a>
+        </div>
+        <div className="footer-bottom">
+          <img src={imgNPlogo} />
+          <p>powered by NewsPicks</p>
+        </div>
       </div>
     </div>
   );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import imgLogo from '../../images/logo.png';
+import imgGp972 from '../../images/Group 972.png';
+import imgCath from '../../images/catch.png';
+import imgGp88 from '../../images/Group 888.png';
 import React from 'react';
 import './header.scss';
 const Header = () => (
-  <header className="header">
-    <div className="nav-top grid-full">
+  <header className="header grid-full">
+    <div className="nav-top ">
       <div className="nav-top__wrap grid">
         <div className="nav-top__logo">
           <img src={imgLogo} />
@@ -23,8 +26,30 @@ const Header = () => (
             <a>Movie</a>
           </li>
         </ul>
-        <a className="entry">Entry</a>
+        <a className="entry">
+          {' '}
+          <img src={imgGp88} />
+          Entry
+        </a>
       </div>
+    </div>
+
+    <div className="header-content">
+      <img src={imgGp972} />
+      <a href={'#'}>オンラインイベント開催決定</a>
+      <div className="header-date">
+        <span>第1回</span>
+        <span>2020.07.07 THU</span>
+      </div>
+    </div>
+    <div className="header-position-img">
+      <img src={imgCath} />
+    </div>
+    <div className="header-watch-new grid">
+      <p>WHAT’S NEW</p>
+      <div className="borders"></div>
+      <p>2020.07.07</p>
+      <a href={'#'}>セッションを更新しました</a>
     </div>
   </header>
 );
