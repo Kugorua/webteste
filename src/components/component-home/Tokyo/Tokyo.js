23fdad953942966f18ffbf608222701069cf37a1
NQphuoc <nguyenqp97@gmail.com>
2020-06-08 02:13:05 +0700

    edit bg by css

--- src/components/component-home/Tokyo/Tokyo.js
+++ src/components/component-home/Tokyo/Tokyo.js
@@ -4,6 +4,8 @@ import imgTokyo from '../../../images/knt_logo@2x.png';
 export default function Tokyo() {
   return (
     <div className="tokyo grid-full">
+      <div className="bg-tokyo-1 grid-full"></div>
+      <div className="bg-tokyo-2 grid-full"></div>
       <div className="tokyo__wrap grid">
         <div className="tokyo__content">
           <p>モビエボとモビエボNEXTは</p>
