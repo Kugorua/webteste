87d7711b0bd71a8a7cd04dbe31004b148003df74
NQphuoc <nguyenqp97@gmail.com>
2020-06-08 15:27:06 +0700

    create-modal meger master

--- src/components/component-home/Tokyo/Tokyo.js
+++ src/components/component-home/Tokyo/Tokyo.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tokyo.scss';
 import imgTokyo from '../../../svgs/tk_knt_logo.svg';
 import btnIcon from '../../../svgs/icon_btn.svg';
+import imgX from '../../../images/x.png';
 
 export default function Tokyo() {
+  let [activ, setActive] = useState(false);
+  const showHideModal = () => {
+    setActive(!activ);
+  };
   return (
     <div className="site_wrap--padding mobiebo">
+      <div className={activ ? 'modal' : 'hide-modal'}>
+        <div className="modal__wrap ">
+          <img className="icon-out" src={imgX} onClick={showHideModal} />
+          <img src={imgTokyo} alt="" style={{ width: 170, height: 70 }} />
+          <div className="modal__content">
+            <h3>TOKYO JUNCTION by KINTOとは？</h3>
+            <p>
+              モビリティ・カンパニーへの変革に取り組むトヨタグループのモビリティサービスブランドKINTO。
+              <br /> KINTOは様々なモビリティ事業者ともに、幅広いモビリティサービスを展開する体制をめざしています。
+              <br />
+              事業者の方々と知り合う場が、東京・日本橋にある「TOKYO JUNCTION」です。
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="mobiebo__box">
         <span>モビエボとモビエボNEXTは</span>
         <img src={imgTokyo} alt="" />
         <span>からお届けします。</span>
       </div>
-      <div className="mobiebo__button">
-        <p >
-          <img src={btnIcon} alt="" />TOKYO JUNCTION by
-        KINTOとは？
+      <div className="mobiebo__button" onClick={showHideModal}>
+        <p>
+          <img src={btnIcon} alt="" />
+          TOKYO JUNCTION by KINTOとは？
         </p>
       </div>
-    </div >
+    </div>
   );
 }
