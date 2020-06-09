c2ee5d4714fcd8e2082ae7141343e9f949c0c812
aguriken <ohagurokenichi@yahoo.co.jp>
2020-06-10 00:14:29 +0900

    add share func

--- src/components/component-home/footer/Footer.js
+++ src/components/component-home/footer/Footer.js
@@ -5,6 +5,7 @@ import mobievo from '../../../svgs/mobievo.svg';
 import tw_icon from '../../../svgs/tw_icon.svg';
 import fb_icon from '../../../svgs/fb_icon.svg';
 import np_logo from '../../../svgs/np_logo.svg';
+import { TwitterShareButton, FacebookShareButton } from 'react-share';
 import './footer.scss';
 
 export default function Footer() {
@@ -15,27 +16,35 @@ export default function Footer() {
           <img src={ft_knt_logo} alt="" />
           <p>
             <img src={icon__buttonmenu} alt="" />
-            <a href="https://corp.kinto-jp.com/">https://corp.kinto-jp.com/</a>
+            <a href="https://corp.kinto-jp.com/" target="_blank" rel="noopener noreferrer">
+              https://corp.kinto-jp.com/
+            </a>
           </p>
         </div>
         <div>
           <img src={mobievo} alt="" />
           <p>
             <img src={icon__buttonmenu} alt="" />
-            <a href="https://newspicks.com/theme-news/3698/">https://newspicks.com/theme-news/3698/</a>
+            <a href="https://newspicks.com/theme-news/3698/" target="_blank" rel="noopener noreferrer">
+              https://newspicks.com/theme-news/3698/
+            </a>
           </p>
         </div>
       </div>
       <div className="footer__social" data-sal="slide-up" data-sal-duration="5s" data-sal-delay="500" ata-sal-easing="ease">
-        <p>
-          SHARE THIS PAGE
-          <img src={tw_icon} alt="" />
-          <img src={fb_icon} alt="" />
-        </p>
+        <div className="footer__social--icons">
+          <p>SHARE THIS PAGE</p>
+          <TwitterShareButton title="モビエボNEXT オンラインイベント開催決定" via="" url="https://westship.newspicks.com/?fbclid=IwAR3UZABbzs313FcDNwV3XhPeykci8sSOIXHdOAncHs-LFm5DfpyXqEVlRO0">
+            <img src={tw_icon} alt="" />
+          </TwitterShareButton>
+          <FacebookShareButton url="https://westship.newspicks.com/?fbclid=IwAR3UZABbzs313FcDNwV3XhPeykci8sSOIXHdOAncHs-LFm5DfpyXqEVlRO0">
+            <img src={fb_icon} alt="" />
+          </FacebookShareButton>
+        </div>
       </div>
       <div className="footer__team" data-sal="slide-up" data-sal-duration="5s" data-sal-delay="500" ata-sal-easing="ease">
         <img src={np_logo} alt="" />
-        <p>powered by NewsPicks</p>
+        <p>Powered by NewsPicks</p>
       </div>
     </div>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -26,7 +26,7 @@ export default function Footer() {
         </div>
         <div className="footer-bottom">
           <img src={imgNPlogo} />
-          <p>Powered by NewsPicks</p>
+          <p>powered by NewsPicks</p>
         </div>
       </div>
     </div>
