a53a4191277036db4dcbc6570a286bc050d9aeb5
NguyenVanTrung1996 <nguyenvantrung2015@gmail.com>
2020-06-10 07:43:51 +0700

    fix footer css

--- src/components/component-home/Tokyo/Tokyo.js
+++ src/components/component-home/Tokyo/Tokyo.js
@@ -10,32 +10,39 @@ export default function Tokyo() {
     setActive(!activ);
   };
   return (
-    <div className="site_wrap--padding mobiebo">
-      <div className={activ ? 'modal' : 'hide-modal'}>
-        <div className="modal__wrap ">
-          <img className="icon-out" src={imgX} onClick={showHideModal} />
-          <img src={imgTokyo} alt="" style={{ width: 170, height: 70 }} />
-          <div className="modal__content">
-            <h3>TOKYO JUNCTION by KINTOとは？</h3>
-            <p>
-              モビリティ・カンパニーへの変革に取り組むトヨタグループのモビリティサービスブランドKINTO。
+    <div className="bg__mobiebo">
+      <div className="bg__mobiebo--1"></div>
+      <div className="bg__mobiebo--2"></div>
+      <div className="bg__mobiebo--3"></div>
+      <div className="bg__mobiebo--4">
+        <div className="site_wrap--padding mobiebo">
+          <div className={activ ? 'modal' : 'hide-modal'}>
+            <div className="modal__wrap ">
+              <img className="icon-out" src={imgX} onClick={showHideModal} />
+              <img src={imgTokyo} alt="" style={{ width: 170, height: 70 }} />
+              <div className="modal__content">
+                <h3>TOKYO JUNCTION by KINTOとは？</h3>
+                <p>
+                  モビリティ・カンパニーへの変革に取り組むトヨタグループのモビリティサービスブランドKINTO。
               <br /> KINTOは様々なモビリティ事業者ともに、幅広いモビリティサービスを展開する体制をめざしています。
               <br />
               事業者の方々と知り合う場が、東京・日本橋にある「TOKYO JUNCTION」です。
             </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="mobiebo__box" data-sal="slide-up" data-sal-delay="100" ata-sal-easing="ease">
-        <span>モビエボとモビエボNEXTは</span>
-        <img src={imgTokyo} alt="" />
-        <span>からお届けします。</span>
-      </div>
-      <div className="mobiebo__button" onClick={showHideModal} data-sal="slide-up" data-sal-delay="100" ata-sal-easing="ease">
-        <p>
-          <img src={btnIcon} alt="" />
+          <div className="mobiebo__box" data-sal="slide-up" data-sal-delay="100" ata-sal-easing="ease">
+            <span>モビエボとモビエボNEXTは</span>
+            <img src={imgTokyo} alt="" />
+            <span>からお届けします。</span>
+          </div>
+          <div className="mobiebo__button" onClick={showHideModal} data-sal="slide-up" data-sal-delay="100" ata-sal-easing="ease">
+            <p>
+              <img src={btnIcon} alt="" />
           TOKYO JUNCTION by KINTOとは？
         </p>
+          </div>
+        </div>
       </div>
     </div>
   );
